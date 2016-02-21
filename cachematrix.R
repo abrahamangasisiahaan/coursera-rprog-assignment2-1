6ea6e878c7fa16ae3a8f7eb0c0e71dc98c776f7b
abrahamangasisiahaan <mangasi.abraham@gmail.com>
2016-02-21 23:43:48 +0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -8,17 +8,18 @@
 # 3. set the value of inverse of the matrix
 # 4. get the value of inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-    inv <- NULL
-    set <- function(y) {
-        x <<- y
-        inv <<- NULL
-    }
-    get <- function() x
-    setinverse <- function(inverse) inv <<- inverse
-    getinverse <- function() inv
-    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(solve) m <<- solve
+      getinverse <- function() m
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
 }
-
 
 # The following function returns the inverse of the matrix. It first checks if
 # the inverse has already been computed. If so, it gets the result and skips the
@@ -27,35 +28,57 @@ makeCacheMatrix <- function(x = matrix()) {
 
 # This function assumes that the matrix is always invertible.
 cacheSolve <- function(x, ...) {
-    inv <- x$getinverse()
-    if(!is.null(inv)) {
-        message("getting cached data.")
-        return(inv)
-    }
-    data <- x$get()
-    inv <- solve(data)
-    x$setinverse(inv)
-    inv
+      inver <- x$getinverse()
+      if(!is.null(inver)) {
+            message("getting cached data")
+            return(inver)
+      }
+      data <- x$get()
+      inver <- solve(data, ...)
+      x$setinverse(inver)
+      inver
 }
 
 ## Sample run:
-## > x = rbind(c(1, -1/4), c(-1/4, 1))
-## > m = makeCacheMatrix(x)
-## > m$get()
-##       [,1]  [,2]
-## [1,]  1.00 -0.25
-## [2,] -0.25  1.00
-
-## No cache in the first run
-## > cacheSolve(m)
-##           [,1]      [,2]
-## [1,] 1.0666667 0.2666667
-## [2,] 0.2666667 1.0666667
-
-## Retrieving from the cache in the second run
-## > cacheSolve(m)
-## getting cached data.
-##           [,1]      [,2]
-## [1,] 1.0666667 0.2666667
-## [2,] 0.2666667 1.0666667
-## > 
+## a <- diag(6,3)
+##      [,1] [,2] [,3]
+## [1,]    6    0    0
+## [2,]    0    6    0
+## [3,]    0    0    6
+## CachedMarix <- makeCacheMatrix(a)
+## cacheSolve(CachedMarix)
+ ##          [,1]      [,2]      [,3]
+## [1,] 0.1666667 0.0000000 0.0000000
+## [2,] 0.0000000 0.1666667 0.0000000
+## [3,] 0.0000000 0.0000000 0.1666667
+##      [,1] [,2] [,3]
+## [1,]  0.2  0.0  0.0
+## [2,]  0.0  0.2  0.0
+## [3,]  0.0  0.0  0.2
+##b <- diag(2,6)
+##b
+##      [,1] [,2] [,3] [,4] [,5] [,6]
+## [1,]    2    0    0    0    0    0
+## [2,]    0    2    0    0    0    0
+## [3,]    0    0    2    0    0    0
+## [4,]    0    0    0    2    0    0
+## [5,]    0    0    0    0    2    0
+## [6,]    0    0    0    0    0    2
+##CachedMarix <- makeCacheMatrix(b)
+##cacheSolve(CachedMarix)     
+##      [,1] [,2] [,3] [,4] [,5] [,6]
+## [1,]  0.5  0.0  0.0  0.0  0.0  0.0
+## [2,]  0.0  0.5  0.0  0.0  0.0  0.0
+## [3,]  0.0  0.0  0.5  0.0  0.0  0.0
+## [4,]  0.0  0.0  0.0  0.5  0.0  0.0
+## [5,]  0.0  0.0  0.0  0.0  0.5  0.0
+## [6,]  0.0  0.0  0.0  0.0  0.0  0.5
+##cacheSolve(CachedMarix)   #getting cached data
+## getting cached data
+##      [,1] [,2] [,3] [,4] [,5] [,6]
+## [1,]  0.5  0.0  0.0  0.0  0.0  0.0
+## [2,]  0.0  0.5  0.0  0.0  0.0  0.0
+## [3,]  0.0  0.0  0.5  0.0  0.0  0.0
+## [4,]  0.0  0.0  0.0  0.5  0.0  0.0
+## [5,]  0.0  0.0  0.0  0.0  0.5  0.0
+## [6,]  0.0  0.0  0.0  0.0  0.0  0.5
